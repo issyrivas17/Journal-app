8cd5191968527c0cb29c4e2cabf8695e6340174f
issyrivas17 <ismeniarivas17@gmail.com>
2022-03-25 22:07:40 -0300

    sidebar-jounrlaentries

--- src/journal/Sidebar.js
+++ src/journal/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { JournalEntries } from './JournalEntries'
 
 export const Sidebar = () => {
   return (
@@ -22,6 +23,10 @@ export const Sidebar = () => {
        <p className='mt-5'>
            New entry 
        </p> 
+
+        <JournalEntries/> 
+
+
 
            
        </div>
