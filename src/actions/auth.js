388ebffd00e46c64e3f785d73f1fedcfb3a0395d
issyrivas17 <ismeniarivas17@gmail.com>
2022-04-14 00:34:36 -0400

    se crea la primera tarea asincrona para ver que todo funcione bien

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,21 @@
 import { types } from "../types/types"
 
+export const startLoginEmailPassword = (email,password) => {
+    return (dispatch) => {
+
+      setTimeout (() => { 
+         
+        dispatch(login (123,'Maria'))
+
+      },3500)
+
+
+
+    }
+}
+
+
+
 
 export const login =  (uid,displayName) => ({
    
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import thunk from 'redux-thunk'
-import { createStore,combineReducers, applyMiddleware} from "redux"
+import { createStore,combineReducers, applyMiddleware,compose} from "redux"
 import { authReducer } from "../reducers/authReducer"; 
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose; 
