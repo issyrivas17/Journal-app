9265d0983699d254941435270b917a2515eef95c
issyrivas17 <ismeniarivas17@gmail.com>
2022-05-30 21:31:45 -0400

    AuthRouter importaciones modificadas correctamente

--- src/components/routers/AuthRouter.js
+++ src/components/routers/AuthRouter.js
@@ -1,33 +1,21 @@
 /* eslint-disable */  
 import React from 'react'
 import { Route, Routes } from 'react-router';
-import { LoginScreen } from '../auth/LoginScreen';
-import { RegisterScreen } from '../auth/RegisterScreen';
- 
-export const AuthRouter = () => {
-    return (
-        <div className="auth__main">
-            <div className="auth__box-container">
-            <Routes>
- 
-            <Route path="/auth/login" element={ 
-     
-            <LoginScreen/>
-     
-            }/>
- 
-            <Route path="/auth/register" element={ 
-     
-            <RegisterScreen/>
-     
-                }/>
-     
- 
- 
-            </Routes>
-            </div>
- 
-      
+
+import { LoginScreen } from '../auth/LoginScreen'; 
+import { RegisterScreen } from '../auth/RegisterScreen'; 
+
+export const AuthRoutes = () => {
+  return (
+    <>
+      <div className="auth__main">
+        <div className="auth__box-container">
+          <Routes>
+            <Route path="/auth/login" element={<LoginScreen />} />
+            <Route path="/auth/register" element={<RegisterScreen />} />
+          </Routes>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
