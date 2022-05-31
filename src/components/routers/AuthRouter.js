8ffaa32b0685b405fc7d149e269f279f3affb9e5
issyrivas17 <ismeniarivas17@gmail.com>
2022-05-30 20:56:53 -0400

    AuthRouter actualizado

--- src/components/routers/AuthRouter.js
+++ src/components/routers/AuthRouter.js
@@ -1,30 +1,33 @@
+/* eslint-disable */  
 import React from 'react'
-import { Redirect, Switch, Route } from 'react-router-dom'
-
-import { LoginScreen } from '../auth/LoginScreen'
-import { RegisterScreen } from '../auth/RegisterScreen'
-
+import { Route, Routes } from 'react-router';
+import { LoginScreen } from '../auth/LoginScreen';
+import { RegisterScreen } from '../auth/RegisterScreen';
+ 
 export const AuthRouter = () => {
-  return (
-    <div className ='auth__main'>
-    <div className='auth__box-container'>
-    <Switch>
-           <Route
-           exact
-            path = "/auth/login"
-            component= {LoginScreen}/>
-
-          <Route
-          exact
-          path = "/auth/register"
-          component={RegisterScreen}
-
-          />
-
-         <Redirect to = "/auth/login"/>
-           </Switch>
-    </div>
-
-    </div>
-  )
+    return (
+        <div className="auth__main">
+            <div className="auth__box-container">
+            <Routes>
+ 
+            <Route path="/auth/login" element={ 
+     
+            <LoginScreen/>
+     
+            }/>
+ 
+            <Route path="/auth/register" element={ 
+     
+            <RegisterScreen/>
+     
+                }/>
+     
+ 
+ 
+            </Routes>
+            </div>
+ 
+      
+        </div>
+    )
 }
